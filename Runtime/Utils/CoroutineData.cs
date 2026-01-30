675ce1aef2df1522e95bbb2f2b7de3c600bec7ed
Wonjeong <wonjeong@huliac.com>
2026-01-30 15:30:35 +0900

    코루틴 캐싱 클래스 추가

---
+++ Runtime/Utils/CoroutineData.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wonjeong.Utils 
+{
+    public static class CoroutineData
+    {
+        #region private 변수
+
+        private static readonly Dictionary<float, WaitForSeconds> DicWaitForSeconds = new Dictionary<float, WaitForSeconds>();
+
+        #endregion
+
+        /// <summary>
+        /// WaitForSeconds 반환 (재사용을 위해 하나의 Dictionary에서 계속 반환해줌)
+        /// </summary>
+        /// <param name="seconds">찾는 시간 초</param>
+        /// <returns></returns>
+        public static WaitForSeconds GetWaitForSeconds(float seconds)
+        {
+            if (!DicWaitForSeconds.ContainsKey(seconds))
+            {
+                DicWaitForSeconds.Add(seconds, new WaitForSeconds(seconds));
+            }
+            
+            return DicWaitForSeconds[seconds];
+        }
+    }
+}
